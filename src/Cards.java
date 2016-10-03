7d4e57903239afe720cbd7fe0c6cf70f948bda36
ivankolev1 <ivankolev1996@gmail.com>
2016-10-04 01:48:18 +0300

    Update Cards.java

--- src/Cards.java
+++ src/Cards.java
@@ -5,52 +5,52 @@ public class Main {
 	public static void main(String[] args) {
 	       Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Въведете карта: ");
+	        System.out.print("Enter card: ");
 
 	        int day = scanner.nextInt();
 
 	        switch (day) {
 	            case A:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 2:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 3:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 4:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 5:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 6:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 7:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 8:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 9:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case 10:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case J:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case Q:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            case K:
-	            	System.out.println("Валидна карта!");
+	            	System.out.println("Valid card!");
 	                break;
 	            default:
-	                System.out.println("Невалидна карта!");
+	                System.out.println("Invalid card!");
 	                break;
 	        }
 	        scanner.close();
