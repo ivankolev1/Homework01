a3aeb07e78aa4ccfe72b27fb4eefe8ff97765b36
ivankolev1 <ivankolev1996@gmail.com>
2016-10-04 01:33:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 	       Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Enter day of the week: ");
+	        System.out.print("Enter number: ");
 
 	        int day = scanner.nextInt();
 
