76b64f73662e4ba6ca20de26775860d8b39dbea6
ivankolev1 <ivankolev1996@gmail.com>
2016-10-04 18:28:23 +0300

    Add files via upload

---
+++ src/Points.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Points
+{
+
+
+public static void main(String[] args) 
+    {
+         {
+	           Scanner points = new Scanner(System.in);
+	           System.out.println("Enter points:");
+	           int num = points.nextInt();
+	           if(num>=1 && num<=9)
+	           {
+	        	   if(num>=1 && num<=3)
+	        	   {
+	        		   num *= 5;
+	        		   System.out.println("Your poins:");
+	        		   System.out.println(num);
+	        	   }else if (num>=4 && num<=6)
+	        	   {
+	        		   num *= 10;
+	        		   System.out.println("Your poins:");
+	        		   System.out.println(num);
+	        	   }else if (num>=7 && num<=9)
+	        	   {
+	        		   num *= 50;
+	        		   System.out.println("Your poins:");
+	        		   System.out.println(num);
+	        	   }
+	           }else System.out.println("Invalid input!");
+         }
+    }
+}
