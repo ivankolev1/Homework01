29e570f45d89b0db66a7e7d0033f9d7773b16f5a
ivankolev1 <ivankolev1996@gmail.com>
2016-10-05 20:36:37 +0300

    Update Points.java

--- src/Points.java
+++ src/Points.java
@@ -14,19 +14,16 @@ public static void main(String[] args)
 	        	   if(num>=1 && num<=3)
 	        	   {
 	        		   num *= 5;
-	        		   System.out.println("Your poins:");
-	        		   System.out.println(num);
+	        		   System.out.println("Your poins:" + num);      		   
 	        	   }else if (num>=4 && num<=6)
 	        	   {
 	        		   num *= 10;
-	        		   System.out.println("Your poins:");
-	        		   System.out.println(num);
+	        		  System.out.println("Your poins:" + num);
 	        	   }else if (num>=7 && num<=9)
 	        	   {
 	        		   num *= 50;
-	        		   System.out.println("Your poins:");
-	        		   System.out.println(num);
-	        	   }
+	        		  System.out.println("Your poins:" + num);
+			   }
 	           }else System.out.println("Invalid input!");
          }
     }
